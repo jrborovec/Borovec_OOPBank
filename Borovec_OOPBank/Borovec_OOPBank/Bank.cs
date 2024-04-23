048cf8148ceac2d2a01ab3098455606132f120d8
jrborovec <167913176+jrborovec@users.noreply.github.com>
2024-04-23 11:59:46 -0700

    Add files via upload

---
+++ Borovec_OOPBank/Borovec_OOPBank/Bank.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borovec_OOPBank
+{
+    public class Bank
+    {
+        private int _balance;
+        public Bank(int balance) 
+        {
+            _balance = balance;
+        }
+
+        //deposit
+        public void Deposit(int deposit)
+        {
+            _balance += deposit;
+            Console.WriteLine("Deposit of $" + deposit + " successful...\n");
+            CheckBalance();
+        }
+        //withdrawal
+        public void Withdrawal(int withdrawal)
+        {
+            if (withdrawal > 500)
+            {
+                Console.WriteLine("Withdrawals are limited to $500 or less, your withdrawal will be adjusted.");
+                withdrawal = 500;            
+            }
+            if (withdrawal <= _balance)
+            {
+                Console.WriteLine("Withdrawal of $" + withdrawal + " successful...\n");
+                _balance -= withdrawal;
+            }
+            else
+            {
+                Console.WriteLine("Account Overdrawn. $" + _balance + " has been withdrawn instead.\n");
+                _balance = 0;
+            }
+            CheckBalance();
+        }
+
+        //check balance
+        public void CheckBalance()
+        {
+            Console.WriteLine("Current Balance: $" + _balance + "\n");
+        }
+    }
+
+}
---
+++ Borovec_OOPBank/Borovec_OOPBank/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Borovec_OOPBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank Bank = new Bank(1000);
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("----- Main Menu -----");
+                Console.WriteLine("1. Make a Deposit");
+                Console.WriteLine("2. Make a Withdrawal");
+                Console.WriteLine("3. Check Current Balance");
+                Console.WriteLine("0. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1: Console.WriteLine("How much would you like to deposit?");
+                        int deposit = Convert.ToInt32(Console.ReadLine());
+                        Bank.Deposit(deposit);
+                        break;
+                    case 2: Console.WriteLine("How much would you like to withdraw?");
+                        int withdrawal = Convert.ToInt32(Console.ReadLine());
+                        Bank.Withdrawal(withdrawal);
+                        break;
+                    case 3: Bank.CheckBalance();
+                        break;
+                    case 0: Console.WriteLine("Exiting...");
+                        isRunning = false;
+                        break;
+                    default: Console.WriteLine("Please enter a valid selection.");
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
